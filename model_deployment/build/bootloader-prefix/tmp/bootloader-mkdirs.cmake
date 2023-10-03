af5e8f7e4acf697d911a14b61d90cd97503321b1
Brian Rodriguez <rdgbrian@gmail.com>
2023-10-03 11:36:05 -0400

    initial commit

---
+++ model_deployment/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Espressif/frameworks/esp-idf-v5.0.4/components/bootloader/subproject"
+  "C:/Users/rdgbr/Documents/University-Stuff/Fall-2023/Capstone-2/bruh/Blogs/ESP-DL/model_deployment/build/bootloader"
+  "C:/Users/rdgbr/Documents/University-Stuff/Fall-2023/Capstone-2/bruh/Blogs/ESP-DL/model_deployment/build/bootloader-prefix"
+  "C:/Users/rdgbr/Documents/University-Stuff/Fall-2023/Capstone-2/bruh/Blogs/ESP-DL/model_deployment/build/bootloader-prefix/tmp"
+  "C:/Users/rdgbr/Documents/University-Stuff/Fall-2023/Capstone-2/bruh/Blogs/ESP-DL/model_deployment/build/bootloader-prefix/src/bootloader-stamp"
+  "C:/Users/rdgbr/Documents/University-Stuff/Fall-2023/Capstone-2/bruh/Blogs/ESP-DL/model_deployment/build/bootloader-prefix/src"
+  "C:/Users/rdgbr/Documents/University-Stuff/Fall-2023/Capstone-2/bruh/Blogs/ESP-DL/model_deployment/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "C:/Users/rdgbr/Documents/University-Stuff/Fall-2023/Capstone-2/bruh/Blogs/ESP-DL/model_deployment/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "C:/Users/rdgbr/Documents/University-Stuff/Fall-2023/Capstone-2/bruh/Blogs/ESP-DL/model_deployment/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
