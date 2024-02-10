12c710b84f3e483c22448715a4fedb00781a6fe6
rdgbrian <121116793+rdgbrian@users.noreply.github.com>
2024-02-10 13:58:47 -0500

    Update sketch_jan17a.ino

--- yolo_experiments/sketch_jan17a/sketch_jan17a.ino
+++ yolo_experiments/sketch_jan17a/sketch_jan17a.ino
@@ -2,8 +2,8 @@
 #include <WiFi.h>
 #include <esp32cam.h>
  
-const char* WIFI_SSID = "tata";
-const char* WIFI_PASS = "Blancuso1";
+const char* WIFI_SSID = "*****";
+const char* WIFI_PASS = "*****";
  
 WebServer server(80);
  
