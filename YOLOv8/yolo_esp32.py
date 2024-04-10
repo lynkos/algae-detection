d22eb60b1e3b87576a16b1264c6d0243fcf6ef6b
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-04-10 19:38:42 -0400

    Add comments to `yolo_esp32.py`

--- YOLOv8/yolo_esp32.py
+++ YOLOv8/yolo_esp32.py
@@ -1,17 +1,25 @@
 from numpy import asarray
-from cv2 import imdecode, IMREAD_COLOR # type: ignore
+from cv2 import imdecode, IMREAD_UNCHANGED, IMREAD_IGNORE_ORIENTATION # type: ignore
 from requests import get
 from yolo_base import showWindow, model
 
 URL = "http://10.0.0.134/snapshot"
 
 while(True):
-    response = get(URL, stream = True).raw.read()
+    # Fetch image from stream at URL
+    response = get(URL, stream = True, allow_redirects = True).raw.read()
 
     if response:
+        # Read image as a numpy array
         img_array = asarray(bytearray(response), dtype = "uint8")
-        image = imdecode(img_array, IMREAD_COLOR)
-        results = model(image)
-        showWindow("ESP32 Stream", image, results)
+        
+        # Decode ndarray to OpenCV image
+        image = imdecode(img_array, IMREAD_UNCHANGED | IMREAD_IGNORE_ORIENTATION)
 
-    break
+        # Algae detection
+        results = model(image)
+        
+        if not results: continue
+        
+        # Show ESP32 stream
+        showWindow("ESP32 Stream", image, results)
