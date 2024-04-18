f2769e0c3e48b20f87aa2249b168c636993a566e
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-04-17 20:18:41 -0400

    Update `base.py`

--- src/detection/base.py
+++ src/detection/base.py
@@ -4,131 +4,100 @@ from torch.backends.mps import is_available as is_mps_available
 from ultralytics import YOLO
 from os import curdir
 from os.path import abspath, join
-from cv2.typing import MatLike
-from cv2 import (VideoCapture, rectangle, putText, namedWindow, imshow,
-                 waitKey, getWindowProperty, getTextSize, destroyAllWindows,
-                 CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FPS,
-                 WND_PROP_VISIBLE, FONT_HERSHEY_SIMPLEX, FILLED, LINE_AA, WINDOW_GUI_EXPANDED)
+from cv2 import (VideoCapture, namedWindow, imshow, waitKey, getWindowProperty,
+                 destroyAllWindows, WND_PROP_VISIBLE, WINDOW_GUI_EXPANDED,
+                 CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FPS)
 
-BOX_COLOR = (255, 0, 0)
-FONT_COLOR = (255, 255, 255)
-THICKNESS = 2
-FONT_SCALE = 1
-"""Bounding box attributes"""
+WIDTH: int = 1280
+HEIGHT: int = 720
+FPS: float = 30.0
+"""Default camera attributes"""
 
-CLASSES = [ "closterium", "microcystis", "nitzschia", "oscillatoria" ]
-"""Default types of algae the model can detect"""
+DEVICE: device = device("mps") if is_mps_available() else device("cuda") if is_cuda_available() else device("cpu")
+"""Default device (GPU or CPU) to run detection model on"""
 
-DEVICE = device("mps") if is_mps_available() else device("cuda") if is_cuda_available() else device("cpu")
-"""Default device (GPU or CPU) to run algae detection model on"""
-
-MODEL_PATH = join(abspath(curdir), "weights", "yolov8n_sahi.pt")
+MODEL_PATH: str = join(abspath(curdir), "weights", "yolov8n_sahi.pt")
 """Default path of custom-trained algae detection model"""
 
+CONFIDENCE: float = 0.25
+"""Default confidence threshold of detection model"""
+
 class Camera:
-    def __init__(self, camera_type: str | int, title: str = "Algae Detector", model_path: str = MODEL_PATH, classes: list[str] = CLASSES, device_type: device | str = DEVICE):
+    def __init__(self,
+                 camera_type: str | int,
+                 title: str = "Algae Detector",
+                 model_path: str = MODEL_PATH,
+                 confidence: float = CONFIDENCE,
+                 device_type: device | str = DEVICE,
+                 width: int = WIDTH,
+                 height: int = HEIGHT,
+                 fps: float = FPS):
         """
         Base class for camera object detection.
 
         Args:
             camera_type (str | int): Which camera to use. Set to streaming URL for ESP32-CAM, `0` for Webcam, `1` for iPhone.
-            title (str): Window title. Defaults to "Algae Detector".
-            model_path (str, optional): Detection model path. Defaults to `MODEL_PATH`.
-            classes (list[str], optional): Types of objects the model can detect. Defaults to `CLASSES`.
+            title (str, optional): Window title. Defaults to "Algae Detector".
+            model_path (str, optional): Detection model's path. Defaults to `MODEL_PATH`.
+            confidence (float, optional): Detection model's confidence threshold. Defaults to `CONFIDENCE`.
             device_type (device | str, optional): Device (GPU or CPU) to run detection model on. Defaults to `DEVICE`.
+            width (int, optional): Camera width. Defaults to `WIDTH`.
+            height (int, optional): Camera height. Defaults to `HEIGHT`.
+            fps (float, optional): Camera FPS. Defaults to `FPS`.
         """
         self.camera: VideoCapture = VideoCapture(camera_type)
-        self.title: str = title
-        self._yolo_model: YOLO = YOLO(model_path, task = "detect")
-        self.classes: list[str] = classes
-        self.device: device | str = device_type
-
-    def run(self, width: int = 1280, height: int = 720, fps: float = 30.0) -> None:
-        """
-        Run camera object detection.
-
-        Args:
-            width (int, optional): Camera width. Defaults to `1280`.
-            height (int, optional): Camera height. Defaults to `720`.
-            fps (float, optional): Camera FPS. Defaults to `30.0`.
-        """
-        # Set camera width, height, and FPS
         self.camera.set(CAP_PROP_FRAME_WIDTH, width)
         self.camera.set(CAP_PROP_FRAME_HEIGHT, height)
         self.camera.set(CAP_PROP_FPS, fps)
+        self.title: str = title
+        self._yolo_model: YOLO = YOLO(model_path, task = "detect")
+        self.confidence: float = confidence
+        self.device: device | str = device_type
 
+    def run(self) -> None:
+        """
+        Run camera object detection.
+        """
         while self.camera.isOpened():
-            # Fetch image from camera
+            # Fetch camera frame
             success, frame = self.camera.read()
 
             if success:
-                # Object detection
-                results = self._yolo_model(frame, stream = True, device = self.device, agnostic_nms = True)
+                # Run algae detection model on the frame
+                results = self._yolo_model(frame,
+                                           stream = True,
+                                           device = self.device,
+                                           stream_buffer = True,
+                                           vid_stride = 5,
+                                           conf = self.confidence,
+                                           iou = 0.5,
+                                           agnostic_nms = True)
 
                 if not results: continue
 
                 # Show camera feed
-                self._showWindow(frame, results)
+                self._showWindow(results)
 
-    def _coordinates(self, frame: MatLike, results: list) -> None:
-        """
-        Get bounding box coordinates and dimensions.
-
-        Args:
-            frame (MatLike): Detected object image.
-            results (list): Model results.
-        """
-        for result in results:
-            for box in result.boxes:
-                # Get bounding box coordinates and dimensions
-                x, y, w, h = box.xyxy[0]
-
-                # Draw bounding box
-                self._drawBoundingBox(frame, f"{self.classes[int(box.cls[0])]} {box.conf[0]:.2f}", int(x), int(y), int(w), int(h))
-
-    def _showWindow(self, frame: MatLike, results: list) -> None:
+    def _showWindow(self, results: list) -> None:
         """
         Show camera feed with bounding boxes over detected objects.
 
         Args:
-            frame (MatLike): Camera frame.
-            results (list): Model results.
+            results (list): Inference results.
         """
-        # Coordinates
-        self._coordinates(frame, results)
-            
         # Create resizable, named window
         namedWindow(self.title, WINDOW_GUI_EXPANDED)
-
-        # Show image
-        imshow(self.title, frame)
-
-        # Exit loop if "q" is pressed or window is closed
-        if (waitKey(1) & 0xFF == ord("q")) or (getWindowProperty(self.title, WND_PROP_VISIBLE) < 1):
-            # Release webcam, if applicable
-            self.camera.release()
-
-            # Close OpenCV windows
-            destroyAllWindows()
-            
-            exit(0)
-
-    def _drawBoundingBox(self, frame: MatLike, label: str, x: int, y: int, w: int, h: int) -> None:
-        """
-        Draw bounding box around detected object.
-
-        Args:
-            frame (MatLike): Detected object image.
-            label (str): Label of detected object.
-            x (int): X-coordinate of bounding box.
-            y (int): Y-coordinate of bounding box.
-            w (int): Width of bounding box.
-            h (int): Height of bounding box.
-        """
-        # Bounding box
-        rectangle(frame, (x, y), (w, h), BOX_COLOR, THICKNESS)
         
-        # Label
-        text_width, text_height = getTextSize(label, FONT_HERSHEY_SIMPLEX, FONT_SCALE, THICKNESS)[0]
-        rectangle(frame, (x, y), (x + text_width, y - (text_height + 10)), BOX_COLOR, FILLED)
-        putText(frame, label, (x, y - 5), FONT_HERSHEY_SIMPLEX, FONT_SCALE, FONT_COLOR, THICKNESS, LINE_AA)
+        for result in results:
+            # Annotate the frame with its result
+            imshow(self.title, result.plot())
+
+            # Exit loop if "q" is pressed or window is closed
+            if (waitKey(1) & 0xFF == ord("q")) or (getWindowProperty(self.title, WND_PROP_VISIBLE) < 1):
+                # Release webcam, if applicable
+                self.camera.release()
+
+                # Close OpenCV windows
+                destroyAllWindows()
+                
+                exit(0)
