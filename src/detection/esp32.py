545df7aeaec1ca211418350e688f1b125ce4d8b4
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-04-18 16:28:42 -0400

    Add width + height to `esp32.py`

--- src/detection/esp32.py
+++ src/detection/esp32.py
@@ -3,5 +3,5 @@ from base import Camera
 URL = "http://10.0.0.114/stream"
 """Stream URL"""
 
-Camera(URL, "ESP32 Stream").run()
+Camera(URL, "ESP32 Stream", width = 320, height = 240).run()
 """Start algae detection program"""
