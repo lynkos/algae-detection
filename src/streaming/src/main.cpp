79f3d65bc3dbdf8b95c4be8230de1ae7ea112cc9
Kiran Brahmatewari <kiwi2mii@gmail.com>
2025-06-12 08:38:56 -0400

    Copy of commit `e93511d`: "Added functions for flash from API" https://github.com/rzeldent/esp32cam-rtsp/commit/e93511d0c854f1124eb7673cbd4416d8089ddb19

--- src/streaming/src/main.cpp
+++ src/streaming/src/main.cpp
@@ -48,10 +48,13 @@ auto param_colorbar = iotwebconf::Builder<iotwebconf::CheckboxTParameter>("cb").
 
 // Camera
 OV2640 cam;
+
 // DNS Server
 DNSServer dnsServer;
+
 // RTSP Server
 std::unique_ptr<rtsp_server> camera_server;
+
 // Web server
 WebServer web_server(80);
 
@@ -61,12 +64,11 @@ IotWebConf iotWebConf(thingName.c_str(), &dnsServer, &web_server, WIFI_PASSWORD,
 // Camera initialization result
 esp_err_t camera_init_result;
 
-void handle_root()
-{
+void handle_root() {
   log_v("Handle root");
+
   // Let IotWebConf test and handle captive portal requests.
-  if (iotWebConf.handleCaptivePortal())
-    return;
+  if (iotWebConf.handleCaptivePortal()) return;
 
   // Format hostname
   auto hostname = "esp32-" + WiFi.macAddress() + ".local";
@@ -83,91 +85,108 @@ void handle_root()
     initResult = "Unknown reason";
 
   moustache_variable_t substitutions[] = {
-      // Version / CPU
-      {"AppTitle", APP_TITLE},
-      {"AppVersion", APP_VERSION},
-      {"BoardType", BOARD_NAME},
-      {"ThingName", iotWebConf.getThingName()},
-      {"SDKVersion", ESP.getSdkVersion()},
-      {"ChipModel", ESP.getChipModel()},
-      {"ChipRevision", String(ESP.getChipRevision())},
-      {"CpuFreqMHz", String(ESP.getCpuFreqMHz())},
-      {"CpuCores", String(ESP.getChipCores())},
-      {"FlashSize", format_memory(ESP.getFlashChipSize(), 0)},
-      {"HeapSize", format_memory(ESP.getHeapSize())},
-      {"PsRamSize", format_memory(ESP.getPsramSize(), 0)},
-      // Diagnostics
-      {"Uptime", String(format_duration(millis() / 1000))},
-      {"FreeHeap", format_memory(ESP.getFreeHeap())},
-      {"MaxAllocHeap", format_memory(ESP.getMaxAllocHeap())},
-      {"NumRTSPSessions", camera_server != nullptr ? String(camera_server->num_connected()) : "RTSP server disabled"},
-      // Network
-      {"HostName", hostname},
-      {"MacAddress", WiFi.macAddress()},
-      {"AccessPoint", WiFi.SSID()},
-      {"SignalStrength", String(WiFi.RSSI())},
-      {"WifiMode", wifi_modes[WiFi.getMode()]},
-      {"IPv4", ipv4.toString()},
-      {"IPv6", ipv6.toString()},
-      {"NetworkState.ApMode", String(iotWebConf.getState() == iotwebconf::NetworkState::ApMode)},
-      {"NetworkState.OnLine", String(iotWebConf.getState() == iotwebconf::NetworkState::OnLine)},
-      // Camera
-      {"FrameSize", String(param_frame_size.value())},
-      {"FrameDuration", String(param_frame_duration.value())},
-      {"FrameFrequency", String(1000.0 / param_frame_duration.value(), 1)},
-      {"JpegQuality", String(param_jpg_quality.value())},
-      {"CameraInitialized", String(camera_init_result == ESP_OK)},
-      {"CameraInitResult", String(camera_init_result)},
-      {"CameraInitResultText", initResult},
-      // Settings
-      {"Brightness", String(param_brightness.value())},
-      {"Contrast", String(param_contrast.value())},
-      {"Saturation", String(param_saturation.value())},
-      {"SpecialEffect", String(param_special_effect.value())},
-      {"WhiteBal", String(param_whitebal.value())},
-      {"AwbGain", String(param_awb_gain.value())},
-      {"WbMode", String(param_wb_mode.value())},
-      {"ExposureCtrl", String(param_exposure_ctrl.value())},
-      {"Aec2", String(param_aec2.value())},
-      {"AeLevel", String(param_ae_level.value())},
-      {"AecValue", String(param_aec_value.value())},
-      {"GainCtrl", String(param_gain_ctrl.value())},
-      {"AgcGain", String(param_agc_gain.value())},
-      {"GainCeiling", String(param_gain_ceiling.value())},
-      {"Bpc", String(param_bpc.value())},
-      {"Wpc", String(param_wpc.value())},
-      {"RawGma", String(param_raw_gma.value())},
-      {"Lenc", String(param_lenc.value())},
-      {"HMirror", String(param_hmirror.value())},
-      {"VFlip", String(param_vflip.value())},
-      {"Dcw", String(param_dcw.value())},
-      {"ColorBar", String(param_colorbar.value())},
-      // RTSP
-      {"RtspPort", String(RTSP_PORT)}};
+    // Version / CPU
+    {"AppTitle", APP_TITLE},
+    {"AppVersion", APP_VERSION},
+    {"BoardType", BOARD_NAME},
+    {"ThingName", iotWebConf.getThingName()},
+    {"SDKVersion", ESP.getSdkVersion()},
+    {"ChipModel", ESP.getChipModel()},
+    {"ChipRevision", String(ESP.getChipRevision())},
+    {"CpuFreqMHz", String(ESP.getCpuFreqMHz())},
+    {"CpuCores", String(ESP.getChipCores())},
+    {"FlashSize", format_memory(ESP.getFlashChipSize(), 0)},
+    {"HeapSize", format_memory(ESP.getHeapSize())},
+    {"PsRamSize", format_memory(ESP.getPsramSize(), 0)},
+
+    // Diagnostics
+    {"Uptime", String(format_duration(millis() / 1000))},
+    {"FreeHeap", format_memory(ESP.getFreeHeap())},
+    {"MaxAllocHeap", format_memory(ESP.getMaxAllocHeap())},
+    {"NumRTSPSessions", camera_server != nullptr ? String(camera_server->num_connected()) : "RTSP server disabled"},
+
+    // Network
+    {"HostName", hostname},
+    {"MacAddress", WiFi.macAddress()},
+    {"AccessPoint", WiFi.SSID()},
+    {"SignalStrength", String(WiFi.RSSI())},
+    {"WifiMode", wifi_modes[WiFi.getMode()]},
+    {"IPv4", ipv4.toString()},
+    {"IPv6", ipv6.toString()},
+    {"NetworkState.ApMode", String(iotWebConf.getState() == iotwebconf::NetworkState::ApMode)},
+    {"NetworkState.OnLine", String(iotWebConf.getState() == iotwebconf::NetworkState::OnLine)},
+
+    // Camera
+    {"FrameSize", String(param_frame_size.value())},
+    {"FrameDuration", String(param_frame_duration.value())},
+    {"FrameFrequency", String(1000.0 / param_frame_duration.value(), 1)},
+    {"JpegQuality", String(param_jpg_quality.value())},
+    {"CameraInitialized", String(camera_init_result == ESP_OK)},
+    {"CameraInitResult", String(camera_init_result)},
+    {"CameraInitResultText", initResult},
+
+    // Settings
+    {"Brightness", String(param_brightness.value())},
+    {"Contrast", String(param_contrast.value())},
+    {"Saturation", String(param_saturation.value())},
+    {"SpecialEffect", String(param_special_effect.value())},
+    {"WhiteBal", String(param_whitebal.value())},
+    {"AwbGain", String(param_awb_gain.value())},
+    {"WbMode", String(param_wb_mode.value())},
+    {"ExposureCtrl", String(param_exposure_ctrl.value())},
+    {"Aec2", String(param_aec2.value())},
+    {"AeLevel", String(param_ae_level.value())},
+    {"AecValue", String(param_aec_value.value())},
+    {"GainCtrl", String(param_gain_ctrl.value())},
+    {"AgcGain", String(param_agc_gain.value())},
+    {"GainCeiling", String(param_gain_ceiling.value())},
+    {"Bpc", String(param_bpc.value())},
+    {"Wpc", String(param_wpc.value())},
+    {"RawGma", String(param_raw_gma.value())},
+    {"Lenc", String(param_lenc.value())},
+    {"HMirror", String(param_hmirror.value())},
+    {"VFlip", String(param_vflip.value())},
+    {"Dcw", String(param_dcw.value())},
+    {"ColorBar", String(param_colorbar.value())},
+
+    // RTSP
+    {"RtspPort", String(RTSP_PORT)}
+  };
 
   web_server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   auto html = moustache_render(index_html_min_start, substitutions);
   web_server.send(200, "text/html", html);
 }
 
-void handle_snapshot()
-{
+#ifdef FLASH_LED_GPIO
+void handle_flash() {
+  log_v("handle_flash");
+  
+  // If no value present, use off, otherwise convert v to integer. Depends on analog resolution for max value
+  auto v = web_server.hasArg("v") ? web_server.arg("v").toInt() : 0;
+
+  // If conversion fails, v = 0
+  analogWrite(FLASH_LED_GPIO, v);
+
+  web_server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  web_server.send(200);
+}
+#endif
+
+void handle_snapshot() {
   log_v("handle_snapshot");
-  if (camera_init_result != ESP_OK)
-  {
+  if (camera_init_result != ESP_OK) {
     web_server.send(404, "text/plain", "Camera is not initialized");
     return;
   }
 
   // Remove old images stored in the frame buffer
   auto frame_buffers = CAMERA_CONFIG_FB_COUNT;
-  while (frame_buffers--)
-    cam.run();
+  while (frame_buffers--) cam.run();
 
   auto fb_len = cam.getSize();
   auto fb = (const char *)cam.getfb();
-  if (fb == nullptr)
-  {
+  if (fb == nullptr) {
     web_server.send(404, "text/plain", "Unable to obtain frame buffer from the camera");
     return;
   }
@@ -180,22 +199,20 @@ void handle_snapshot()
 
 #define STREAM_CONTENT_BOUNDARY "123456789000000000000987654321"
 
-void handle_stream()
-{
+void handle_stream() {
   log_v("handle_stream");
-  if (camera_init_result != ESP_OK)
-  {
+  if (camera_init_result != ESP_OK) {
     web_server.send(404, "text/plain", "Camera is not initialized");
     return;
   }
 
   log_v("starting streaming");
+
   // Blocks further handling of HTTP server until stopped
   char size_buf[12];
   auto client = web_server.client();
   client.write("HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: multipart/x-mixed-replace; boundary=" STREAM_CONTENT_BOUNDARY "\r\n");
-  while (client.connected())
-  {
+  while (client.connected()) {
     client.write("\r\n--" STREAM_CONTENT_BOUNDARY "\r\n");
     cam.run();
     client.write("Content-Type: image/jpeg\r\nContent-Length: ");
@@ -209,14 +226,15 @@ void handle_stream()
   log_v("stopped streaming");
 }
 
-esp_err_t initialize_camera()
-{
+esp_err_t initialize_camera() {
   log_v("initialize_camera");
 
   log_i("Frame size: %s", param_frame_size.value());
   auto frame_size = lookup_frame_size(param_frame_size.value());
+
   log_i("JPEG quality: %d", param_jpg_quality.value());
   auto jpeg_quality = param_jpg_quality.value();
+
   log_i("Frame duration: %d ms", param_frame_duration.value());
   const camera_config_t camera_config = {
     .pin_pwdn = CAMERA_CONFIG_PIN_PWDN,         // GPIO pin for camera power down line
@@ -253,11 +271,10 @@ esp_err_t initialize_camera()
   return cam.init(camera_config);
 }
 
-void update_camera_settings()
-{
+void update_camera_settings() {
   auto camera = esp_camera_sensor_get();
-  if (camera == nullptr)
-  {
+
+  if (camera == nullptr) {
     log_e("Unable to get camera sensor");
     return;
   }
@@ -286,43 +303,50 @@ void update_camera_settings()
   camera->set_colorbar(camera, param_colorbar.value());
 }
 
-void start_rtsp_server()
-{
+void start_rtsp_server() {
   log_v("start_rtsp_server");
   camera_server = std::unique_ptr<rtsp_server>(new rtsp_server(cam, param_frame_duration.value(), RTSP_PORT));
+  
   // Add RTSP service to mDNS
   // HTTP is already set by iotWebConf
   MDNS.addService("rtsp", "tcp", RTSP_PORT);
 }
 
-void on_connected()
-{
+void on_connected() {
   log_v("on_connected");
+  
   // Start the RTSP Server if initialized
-  if (camera_init_result == ESP_OK)
-    start_rtsp_server();
-  else
-    log_e("Not starting RTSP server: camera not initialized");
+  if (camera_init_result == ESP_OK) start_rtsp_server();
+  
+  else log_e("Not starting RTSP server: camera not initialized");
 }
 
-void on_config_saved()
-{
+void on_config_saved() {
   log_v("on_config_saved");
   update_camera_settings();
 }
 
-void setup()
-{
+void setup() {
   // Disable brownout
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
 
 #ifdef USER_LED_GPIO
   pinMode(USER_LED_GPIO, OUTPUT);
   digitalWrite(USER_LED_GPIO, !USER_LED_ON_LEVEL);
 #endif
 
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
+#ifdef FLASH_LED_GPIO
+  pinMode(FLASH_LED_GPIO, OUTPUT);
+
+  // Set resolution to 8 bits
+  analogWriteResolution(8);
+
+  // Turn flash led off
+  analogWrite(FLASH_LED_GPIO, 0);
+#endif
 
 #ifdef ARDUINO_USB_CDC_ON_BOOT
   // Delay for USB to connect/settle
@@ -364,8 +388,8 @@ void setup()
   param_group_camera.addItem(&param_vflip);
   param_group_camera.addItem(&param_dcw);
   param_group_camera.addItem(&param_colorbar);
-  iotWebConf.addParameterGroup(&param_group_camera);
 
+  iotWebConf.addParameterGroup(&param_group_camera);
   iotWebConf.getApTimeoutParameter()->visible = true;
   iotWebConf.setConfigSavedCallback(on_config_saved);
   iotWebConf.setWifiConnectionCallback(on_connected);
@@ -375,11 +399,9 @@ void setup()
   iotWebConf.init();
 
   // Try to initialize 3 times
-  for (auto i = 0; i < 3; i++)
-  {
+  for (auto i = 0; i < 3; i++) {
     camera_init_result = initialize_camera();
-    if (camera_init_result == ESP_OK)
-    {
+    if (camera_init_result == ESP_OK) {
       update_camera_settings();
       break;
     }
@@ -391,19 +413,23 @@ void setup()
 
   // Set up required URL handlers on the web server
   web_server.on("/", HTTP_GET, handle_root);
-  web_server.on("/config", []
-                { iotWebConf.handleConfig(); });
+  web_server.on("/config", [] { iotWebConf.handleConfig(); });
+  
   // Camera snapshot
   web_server.on("/snapshot", HTTP_GET, handle_snapshot);
+  
   // Camera stream
   web_server.on("/stream", HTTP_GET, handle_stream);
 
-  web_server.onNotFound([]()
-                        { iotWebConf.handleNotFound(); });
+#ifdef FLASH_LED_GPIO
+  // Flash led
+  web_server.on("/flash", HTTP_GET, handle_flash);
+#endif
+
+  web_server.onNotFound([]() { iotWebConf.handleNotFound(); });
 }
 
-void loop()
-{
+void loop() {
   iotWebConf.doLoop();
 
   if (camera_server)
